ea962be4d57359472ed118f6cbcf123bddd0d7bf
Subham <subhammandal01@gmail.com>
2022-07-01 17:19:04 +0000

    updated eks cluster

---
+++ eksctl-cluster/cluster_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export USERID=$(id -u)
+export PATH="$PATH:/usr/local/bin"
+export GROUPID=$(id -g)
+cd $(dirname $0)
+
+docker-compose -f compose.yaml \
+    run --rm -w "$WORKSPACE" \
+    --name eks-${BUILD_NUMBER} eksctl create cluster -f eks-cluster.yaml
