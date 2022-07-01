2fc1e1f7327114d74453098f8fbaa80fa87cd239
Subham <subhammandal01@gmail.com>
2022-07-01 17:07:05 +0000

    update dockerfile

--- images/image-build.sh
+++ images/image-build.sh
@@ -8,4 +8,4 @@ REPO=$ECR_REPO
 grep 'region' ~/.aws/config > /dev/null 2>&1 || aws configure set default.region $REGION
 
 REGISTRY="$(aws sts get-caller-identity --query 'Account' --output text).dkr.ecr.${REGION}.amazonaws.com"
-docker build -t $REGISTRY/$REPO:$TAG  --build-arg MESSAGE="$MESSAGE" .
+docker build -t $REGISTRY/$REPO:$TAG  --build-arg MESSAGE="$MESSAGE" -f $(dirname $0)/images/Dockerfile .
