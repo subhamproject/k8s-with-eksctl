059b9262b5ea1b74a25c3de87b33aa8fd2ceb95c
Subham <subhammandal01@gmail.com>
2022-07-17 04:38:43 +0000

    fixing bugs

--- vpa/cleanup.sh
+++ vpa/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 kubectl delete -f deployment.yaml
-kubectl delte -f vpa.yal
-kubectl delte -f service.yaml
+kubectl delete -f vpa.yal
+kubectl delete -f service.yaml
 kubectl delete -f metrics.yaml
